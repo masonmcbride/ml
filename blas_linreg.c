f0aec917447ca23eaaf5590aa51b04c2de1f1560
masonmcbride <mcbridedmason@gmail.com>
2025-01-11 02:11:16 -0800

    another pass

--- blas_linreg.c
+++ blas_linreg.c
@@ -5,10 +5,17 @@ void dgeqp3(int m, int n, double** a, int lda, int* jpvt,
             double* tau, double* work, int lwork, int info) {
 /*  DGEQP3 computes a QR factorization with column pivoting of a
     matrix A:  A*P = Q*R  using Level 3 BLAS. */
+    int inb = 1, inbmin = 2, ixover = 3;
+    int fjb, iws, j, jb, lwkopt, minmn, minws, na, nb, 
+        nbmin, nfxd, nx, sm, sminmn, sn, topbmn;
+    
+    // external dgeqrf, dlaqp2, dlaqps, dormqr, dwap (BLAS), xerbla
+    // int ilaenv, double, dnrm2
+
 
 /* Do Column Pivoting */
-    int nfxd = 0;
-    for (int j = 0; j < n; j++) {
+    nfxd = 0;
+    for (j = 0; j < n; j++) {
         if (jpvt[j] != 0) {
             if (j != nfxd) {
                 bli_dswapv(m, a[1,j],1, a[1,nfxd],1);
@@ -29,9 +36,9 @@ void dgeqp3(int m, int n, double** a, int lda, int* jpvt,
 * and update remaining columns
 */
     if (nfxd > 0) {
-        int na = min( m, nfxd );
+        na = min( m, nfxd );
         dgeqrf( m, na, a, lda, tau, work, lwork, info);
-        int iws = max( iws, (int)work[1] );
+        iws = max( iws, (int)work[1] );
         if (na < n) {
             dormqr('LEFT', 'Transpose', m, n-na, na, a, lda, tau,
                     a[1,na+1], lda, work, lwork, info );
@@ -41,17 +48,16 @@ void dgeqp3(int m, int n, double** a, int lda, int* jpvt,
 
 /* Factorize free columns */
 
-    int minmn = min( m, n );
+    minmn = min( m, n );
     if (nfxd < minmn) {
-        int sm = m - nfxd;
-        int sn = n - nfxd;
-        int sminmn = minmn - nfxd;
+        sm = m - nfxd;
+        sn = n - nfxd;
+        sminmn = minmn - nfxd;
 
         // Determine the block size
-        int inb = 1, inbmin = 2, ixover = 3;
-        int nb = ilaev( inb, 'DGEQRF', ' ', sm, sn, -1, -1 );
-        int nbmin = 2;
-        int nx = 0;
+        nb = ilaev( inb, 'DGEQRF', ' ', sm, sn, -1, -1 );
+        nbmin = 2;
+        nx = 0;
 
         if (nb > 1 && nb < sminmn) {
             // Determine when to cross over from blocked to unblocked code
@@ -59,7 +65,7 @@ void dgeqp3(int m, int n, double** a, int lda, int* jpvt,
 
             if ( nx < sminmn ) {
                 // Determine if workspace is large enough for blocked code
-                int minws = 2*sn + ( sn+1 )*nb;
+                minws = 2*sn + ( sn+1 )*nb;
                 iws = max( iws, minws );
                 if ( lwork < minws ) {
                     // Not enough workspace to use optimal NB: Reduce NB
@@ -71,20 +77,18 @@ void dgeqp3(int m, int n, double** a, int lda, int* jpvt,
         }
 
 /* Initialize partial column norms. The first N elements of work store the exact column norms. */
-    for (int j = nfxd; j < n; j++) {
+    for (j = nfxd; j < n; j++) {
         double norm = bli_dnrm2v( sm, a[nfxd+1,j], 1 ); 
         work[n+j] = work[j];
     }  
 
     if ( nb >= nbmin && nb < sminmn && nx < sminmn ) {
-        int j = nfxd + 1;
-        int topbmn = minmn - nx;
-        int jb;
+        j = nfxd + 1;
+        topbmn = minmn - nx;
         while ( j <= topbmn ) {
             jb = min( nb, topbmn-j+1 );
 
             // Factorize jb columns amoung columns j:n
-            int fjb;
             dlaqps( m, n-j+1, j-1, jb, fjb, a[1,j], lda, 
                     jpvt[j], tau[j], work[j], work[n+j],
                     work[2*n+1], work[2*n+jb+1], n-j+1 );
@@ -92,7 +96,7 @@ void dgeqp3(int m, int n, double** a, int lda, int* jpvt,
             j += fjb;
         }
     } else {
-        int j = nfxd + 1;
+        j = nfxd + 1;
     }
 
     if ( j <= minmn ) {
